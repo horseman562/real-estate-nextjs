3d39b461e99185950afd28db6b2484e4ec4a57c6
horseman562 <deathreaper754@gmail.com>
2022-05-25 06:40:13 +0800

    Update search.js

--- pages/search.js
+++ pages/search.js
@@ -1,21 +1,21 @@
-
 import { useState } from 'react';
 import { useRouter } from 'next/router';
 import Image from 'next/image'
 import { Flex, Box, Text, Icon } from '@chakra-ui/react';
 import { BsFilter } from 'react-icons/bs';
-import SearchFilters from '../components/SearchFilters';
-import noresult from '../assets/images/noresult.svg'
-import Property from '../components/Property';
-import { fetchApi, baseUrl } from '../utils/fetchApi';
 
-const search = ({properties}) => {
-    const [searchFilters, setSearchFilters] = useState(false);
-    const router = useRouter();
+import Property from '../components/Property';
+import SearchFilters from '../components/SearchFilters';
+import { baseUrl, fetchApi } from '../utils/fetchApi';
+import noresult from '../assets/images/noresult.svg'
+
+const Search = ({ properties }) => {
+  const [searchFilters, setSearchFilters] = useState(false);
+  const router = useRouter();
 
   return (
     <Box>
-        <Flex
+      <Flex
         onClick={() => setSearchFilters(!searchFilters)}
         cursor='pointer'
         bg='gray.100'
@@ -44,12 +44,10 @@ const search = ({properties}) => {
         </Flex>
       )}
     </Box>
-  )
-}
+  );
+};
 
-export default search
-
-export const getServerSideProps = async ({query}) => {
+export async function getServerSideProps({ query }) {
   const purpose = query.purpose || 'for-rent';
   const rentFrequency = query.rentFrequency || 'yearly';
   const minPrice = query.minPrice || '0';
@@ -62,10 +60,12 @@ export const getServerSideProps = async ({query}) => {
   const categoryExternalID = query.categoryExternalID || '4';
 
   const data = await fetchApi(`${baseUrl}/properties/list?locationExternalIDs=${locationExternalIDs}&purpose=${purpose}&categoryExternalID=${categoryExternalID}&bathsMin=${bathsMin}&rentFrequency=${rentFrequency}&priceMin=${minPrice}&priceMax=${maxPrice}&roomsMin=${roomsMin}&sort=${sort}&areaMax=${areaMax}`);
-  
-   return {
+
+  return {
     props: {
-      properties: data.hits
+      properties: data?.hits,
     },
-   }
-  }
+  };
+}
+
+export default Search;
